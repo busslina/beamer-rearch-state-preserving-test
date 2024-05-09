f15f066136c5463174b4d9baa740f1f91d69cd20
busslina <busslina@gmail.com>
2024-05-09 11:58:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# beamer_rearch_state_preserving_test
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/beamer_rearch_state_preserving_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.beamer_rearch_state_preserving_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
